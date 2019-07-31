80ec4c1715693c7d8ad7afda14056e1a06d52ef1
An Le <an.le@i-on.net>
2019-07-31 17:05:59 +0700

    Separate photo component, add default image

--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -2,25 +2,14 @@ import React, { Component } from "react";
 import { FlatList, StyleSheet, Image, Dimensions, TouchableOpacity } from 'react-native';
 
 export default class PhotoGallery extends Component {
-  getImage(src) {
-    return {
-      uri: src
-    }
-  }
-
   render() {
     return (
       <FlatList
         data={this.props.images}
         numColumns={2}
-        renderItem={({item}) =>
-          <TouchableOpacity>
-            <Image
-              source={this.getImage(item.value)}
-              style={styles.image}
-              resizeMode="cover"
-            />
-          </TouchableOpacity>
+        renderItem={({item}) =>(
+          <Photo source={item.value} />
+          )
         }
         keyExtractor={(item, index) => index.toString()}
       />
@@ -28,6 +17,21 @@ export default class PhotoGallery extends Component {
   }
 }
 
+const Photo = (props) => {
+  const src = { uri: props.source };
+  return (
+    <TouchableOpacity>
+      <Image
+        source={src}
+        style={styles.image}
+        resizeMode="cover"
+        defaultSource={defaultImage}
+      />
+    </TouchableOpacity>
+  )
+};
+
+const defaultImage = require('../../assets/default-fallback-image.png');
 const imgWidth = Dimensions.get('window').width / 2;
 const styles = StyleSheet.create({
   image: {
