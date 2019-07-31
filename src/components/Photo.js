9e25a6b8fadbc6e77cf323c4e65a2ec5b1cf8172
An Le <an.le@i-on.net>
2019-07-31 19:54:14 +0700

    navigate to details screen

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import {Dimensions, Image, StyleSheet, TouchableOpacity} from "react-native";
 import { withNavigation } from 'react-navigation'
+import defaultImage from '../../assets/default-fallback-image.png';
 
-const defaultImage = require('../../assets/default-fallback-image.png');
+// const defaultImage = require('../../assets/default-fallback-image.png');
 const imgWidth = Dimensions.get('window').width / 2;
 
 class Photo extends Component {
--- src/screens/Details.js
+++ src/screens/Details.js
@@ -5,6 +5,7 @@ import AutoHeightImage from "react-native-auto-height-image";
 import BottomTab from "../components/BottomTab";
 import { Ionicons } from '@expo/vector-icons';
 
+const defaultImage = require('../../assets/default-fallback-image.png');
 const dummyUser = require('../testData/dummyUser');
 const fullWidth = Dimensions.get('window').width;
 
@@ -36,13 +37,12 @@ export default class Details extends Component {
     const { navigation } = this.props;
     const itemId = navigation.getParam('id', '');
     const foundImages = dummyUser.photos.data.filter(item => item.id === itemId);
-
     const imageSrc = { uri: foundImages[0].value };
 
     return (
       <SafeAreaView style={ styles.container }>
         <View style={ styles.body }>
-          <AutoHeightImage source={ imageSrc } width={ fullWidth }/>
+          <AutoHeightImage source={ imageSrc } width={ fullWidth } fallBackSource={ defaultImage }/>
         </View>
         <View style={ styles.footer }>
           <BottomTab />
